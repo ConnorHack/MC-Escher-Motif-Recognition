943246fa87cd616f23b3a8f93122274a6da23391
Connor Hack <Connor Hack>
2017-05-13 13:21:22 -0400

    Fixed git conflicts

--- cc2frame.m
+++
@@ -1,9 +0,0 @@
-function imgs = cc2frame(I,cc)
-
-    imgs = {};
-
-    for i = 1:numel(cc)
-        imgs{i} = ind2frame( I , cc{i} );
-    end
-
-end
--- src/README.md
+++
@@ -1,2 +0,0 @@
-# MC-Escher-Motif-Recognition
-Categorization of motif groups using MC Escher's works
